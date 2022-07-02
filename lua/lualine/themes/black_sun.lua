338e65bb17a005c770ee1aaad79aabbed153a6da
Andrew Mustea <amustea224@gmail.com>
2022-07-02 01:45:43 -0700

    lualine: update colornames

--- lua/lualine/themes/black_sun.lua
+++ lua/lualine/themes/black_sun.lua
@@ -1,10 +1,10 @@
 local colors = {
   black_gray   = '#151515',
   dark_gray    = '#262626',
-  gray_blue    = '#4f5b66',
+  blue_gray    = '#4f5b66',
   gray         = '#666666',
-  white        = '#d8dee9',
   white_gray   = '#bbbbbb',
+  white        = '#d8dee9',
   red          = '#800000',
   green        = '#144714',
   blue         = '#08467c',
@@ -14,31 +14,31 @@ return {
   normal = {
     a = {bg = colors.blue,       fg = colors.white,       gui = 'bold'},
     b = {bg = colors.dark_gray,  fg = colors.white_gray},
-    c = {bg = colors.black_gray, fg = colors.gray_blue}
+    c = {bg = colors.black_gray, fg = colors.blue_gray}
   },
   insert = {
     a = {bg = colors.green,      fg = colors.white_gray,  gui = 'bold'},
     b = {bg = colors.dark_gray,  fg = colors.white_gray},
-    c = {bg = colors.black_gray, fg = colors.gray_blue}
+    c = {bg = colors.black_gray, fg = colors.blue_gray}
   },
   visual = {
     a = {bg = colors.red,        fg = colors.white_gray,  gui = 'bold'},
     b = {bg = colors.dark_gray,  fg = colors.white_gray},
-    c = {bg = colors.black_gray, fg = colors.gray_blue}
+    c = {bg = colors.black_gray, fg = colors.blue_gray}
   },
   replace = {
     a = {bg = colors.orange,     fg = colors.white_gray,  gui = 'bold'},
     b = {bg = colors.dark_gray,  fg = colors.white_gray},
-    c = {bg = colors.black_gray, fg = colors.gray_blue}
+    c = {bg = colors.black_gray, fg = colors.blue_gray}
   },
   command = {
     a = {bg = colors.blue,       fg = colors.white,       gui = 'bold'},
     b = {bg = colors.dark_gray,  fg = colors.white_gray},
-    c = {bg = colors.black_gray, fg = colors.gray_blue}
+    c = {bg = colors.black_gray, fg = colors.blue_gray}
   },
   inactive = {
     a = {bg = colors.dark_gray,  fg = colors.gray,        gui = 'bold'},
-    b = {bg = colors.black_gray, fg = colors.gray_blue},
-    c = {bg = colors.black_gray, fg = colors.gray_blue}
+    b = {bg = colors.black_gray, fg = colors.blue_gray},
+    c = {bg = colors.black_gray, fg = colors.blue_gray}
   }
 }
